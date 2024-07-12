68ebe48efed1b3489c21b3fe01ca91904242a63b
james-mitchell-storelab <james.m@storelab.app>
2024-07-12 17:33:46 +0100

    add plan transfer details

--- src/Objects/Transfers/PlanDetails.php
+++ src/Objects/Transfers/PlanDetails.php
@@ -63,6 +63,29 @@ final class PlanDetails extends AbstractTransfer
     public $returnUrl;
 
     /**
+     * The total number of billing intervals to which the discount will be applied.
+     * Must be greater than 0.
+     * The discount will be applied to an indefinite number of billing intervals if this value is left blank.
+     *
+     * @var int|null
+     */
+    public $discountDuration;
+
+    /**
+     * The monetary value of a discount.
+     *
+     * @var float|null
+     */
+    public $discountAmount;
+
+    /**
+     * The percentage value of a discount.
+     *
+     * @var float|null
+     */
+    public $discountPercentage;
+
+    /**
      * {@inheritdoc}
      */
     public function toArray(): array
@@ -76,6 +99,9 @@ final class PlanDetails extends AbstractTransfer
             'return_url' => $this->returnUrl,
             'terms' => $this->terms,
             'capped_amount' => $this->cappedAmount,
+            'discount_duration' => $this->discountDuration,
+            'discount_amount' => $this->discountAmount,
+            'discount_percentage' => $this->discountPercentage,
         ];
     }
 }
